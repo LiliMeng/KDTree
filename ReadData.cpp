e474b1c4e73d3fcd32d377849c668ce91a3997da
LiliMeng <lilimeng1103@gmail.com>
2015-10-11 12:48:31 -0700

    Create ReadData.cpp

---
+++ ReadData.cpp
@@ -0,0 +1,21 @@
+/**
+ * File: ReadData.cpp
+ * Author: Lili Meng (lilimeng1103@gmail.com)
+ * read the line and column data from .txt file
+ */
+
+#include "ReadData.h"
+
+
+int main() {
+
+    readData l("sample_data.txt");
+
+    cout<<l.get_num_of_dimensions()<<endl;
+    cout<<l.get_num_of_elements()<<endl;
+
+    l.printDataID();
+
+
+    return 0;
+}
